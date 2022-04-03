e27777878c7398aef4d7690eab8594e610ebd58f
Ilham <ilhamsyaputra25@gmail.com>
2022-04-03 22:45:52 +0700

    inital commit

---
+++ src/coba.js
@@ -0,0 +1,4 @@
+const { nanoid } = require('nanoid');
+
+const id = `song-${nanoid(16)}`;
+console.log(id);
---
+++ src/server.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const AlbumService = require('./services/postgres/AlbumService');
+const AlbumValidator = require('./validator/album');
+const songs = require('./api/songs');
+const SongService = require('./services/postgres/SongService');
+const SongValidator = require('./validator/song');
+
+const init = async () => {
+  const albumService = new AlbumService();
+  const songService = new SongService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumService,
+        validator: AlbumValidator,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        servoce: songService,
+        validator: SongValidator,
+      },
+    },
+  ]);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
