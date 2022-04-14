18ce2f78a4039fd9e22a8d62fb131b1b2b418fd9
Ilham <ilhamsyaputra25@gmail.com>
2022-04-15 03:34:12 +0700

    Playlist implemented

---
+++ src/api/playlists/handler.js
@@ -0,0 +1,167 @@
+/* eslint-disable no-underscore-dangle */
+const {
+  successResponse,
+  failResponse,
+  serverErrorResponse,
+} = require('../../utils/responseHandler');
+const ClientError = require('../../exceptions/ClientError');
+
+class PlaylistsHandler {
+  constructor(service, songsService, validator) {
+    this._service = service;
+    this._validator = validator;
+    this._songsService = songsService;
+
+    this.postPlaylistHandler = this.postPlaylistHandler.bind(this);
+    this.getPlaylistsHandler = this.getPlaylistsHandler.bind(this);
+    this.deletePlaylistHandler = this.deletePlaylistHandler.bind(this);
+    this.postSongByPlaylistIdHandler = this.postSongByPlaylistIdHandler.bind(this);
+    this.getSongsByPlaylistIdHandler = this.getSongsByPlaylistIdHandler.bind(this);
+    this.deleteSongsByPlaylistIdHandler = this.deleteSongsByPlaylistIdHandler.bind(this);
+  }
+
+  async postPlaylistHandler(request, h) {
+    try {
+      this._validator.validatePostPlaylistPayloadSchema(request.payload);
+
+      const { name } = request.payload;
+      const { id: credentialId } = request.auth.credentials;
+
+      const _playlistId = await this._service.addPlaylist({ name, credentialId });
+
+      return successResponse(h, {
+        data: {
+          playlistId: _playlistId,
+        },
+        responseCode: 201,
+      });
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return failResponse(h, error);
+      }
+
+      // server error
+      console.error(error);
+      return serverErrorResponse(h);
+    }
+  }
+
+  async getPlaylistsHandler(request, h) {
+    try {
+      const { id: credentialId } = request.auth.credentials;
+      const _playlists = await this._service.getPlaylists(credentialId);
+
+      return successResponse(h, {
+        data: {
+          playlists: _playlists,
+        },
+      });
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return failResponse(h, error);
+      }
+
+      // server error
+      return serverErrorResponse(h);
+    }
+  }
+
+  async deletePlaylistHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
+
+      await this._service.verifyPlaylistOwner(id, credentialId);
+      await this._service.deletePlaylistById(id);
+
+      return successResponse(h, {
+        message: 'Playlist berhasil dihapus',
+      });
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return failResponse(h, error);
+      }
+
+      // server error
+      return serverErrorResponse(h);
+    }
+  }
+
+  async postSongByPlaylistIdHandler(request, h) {
+    try {
+      this._validator.validatePostAddSongToPlaylistPayloadSchema(request.payload);
+
+      const { id } = request.params;
+      const { songId } = request.payload;
+      const { id: credentialId } = request.auth.credentials;
+
+      await this._songsService.getSongById(songId);
+      await this._service.verifyPlaylistAccess(id, credentialId);
+      await this._service.addSongToPlaylist(id, songId);
+
+      return successResponse(h, {
+        message: 'Lagu berhasil ditambahkan ke playlist',
+        responseCode: 201,
+      });
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return failResponse(h, error);
+      }
+
+      // server error
+      return serverErrorResponse(h);
+    }
+  }
+
+  async getSongsByPlaylistIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const { id: credentialId } = request.auth.credentials;
+
+      await this._service.verifyPlaylistAccess(id, credentialId);
+      const playlistInfo = await this._service.getPlaylistInfo(id);
+      const songs = await this._service.getSongsByPlaylistId(id);
+      return successResponse(h, {
+        data: {
+          playlist: {
+            id: playlistInfo.id,
+            name: playlistInfo.name,
+            username: playlistInfo.username,
+            songs,
+          },
+        },
+      });
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return failResponse(h, error);
+      }
+
+      // server error
+      return serverErrorResponse(h);
+    }
+  }
+
+  async deleteSongsByPlaylistIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const { songId } = request.payload;
+      const { id: credentialId } = request.auth.credentials;
+
+      await this._service.verifyPlaylistOwner(id, credentialId);
+      await this._service.deleteSongFromPlaylistBySongId(songId, id);
+
+      return successResponse(h, {
+        message: 'Lagu berhasil dihapus dari playlist',
+      });
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return failResponse(h, error);
+      }
+
+      // server error
+      return serverErrorResponse(h);
+    }
+  }
+}
+
+module.exports = PlaylistsHandler;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+
+// albums
 const albums = require('./api/albums');
 const AlbumService = require('./services/postgres/AlbumService');
 const AlbumValidator = require('./validator/album');
@@ -22,11 +25,17 @@ const AuthenticationsService = require('./services/postgres/AuthenticationsServi
 const AuthenticationsValidator = require('./validator/authentications');
 const TokenManager = require('./tokenize/TokenManager');
 
+// playlists
+const playlists = require('./api/playlists');
+const PlaylistsService = require('./services/postgres/PlaylistsService');
+const PlaylistsValidator = require('./validator/playlists');
+
 const init = async () => {
   const albumService = new AlbumService();
   const songService = new SongService();
   const usersService = new UserService();
   const authenticationsService = new AuthenticationsService();
+  const playlistsService = new PlaylistsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -36,6 +45,29 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // authentikasi Jwt
+  server.auth.strategy('openMusic_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
@@ -67,6 +99,14 @@ const init = async () => {
         usersService,
         tokenManager: TokenManager,
         validator: AuthenticationsValidator,
+      },
+    },
+    {
+      plugin: playlists,
+      options: {
+        service: playlistsService,
+        songsService: songService,
+        validator: PlaylistsValidator,
       },
     },
   ]);
